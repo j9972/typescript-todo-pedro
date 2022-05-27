ad7a633686894a03d5bb7f9d243700034de4ea42
jung su young <j9972@naver.com>
2022-05-27 15:53:38 +0900

    set up with react & typescript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const name : string = 'Jung'
+  return (
+    <div className="App">
+      {name}
+    </div>
+  );
+}
+
+export default App;
